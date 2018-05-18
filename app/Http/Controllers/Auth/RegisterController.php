a1d0cf0b440ed56d632bb0d6663b97e34d912e7b
gkity19677 <kity19677@gmail.com>
2018-05-18 09:10:08 +0800

    註冊

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,6 +65,8 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'account' => $data['account'],
+            'sex' => $data['sex'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
