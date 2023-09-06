8c3d0bd9b741d68642baf9a247b039a6a2f79a53
gkity19677 <kity19677@gmail.com>
2023-09-06 17:51:53 +0800

    route middleware auth check fixed

--- routes/web.php
+++ routes/web.php
@@ -11,41 +11,57 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
 Auth::routes();
 
 
-Route::get('/index', 'HomeController@index');
 
-Route::get('/share', 'DreamSharingController@share');
-Route::get('/share_result/{id}', 'DreamSharingController@share_result');
-Route::post('/share_result/{id}', 'DreamSharingController@message');
-Route::get('/keep', 'DreamSharingController@keep');
-Route::post('/keep', 'DreamSharingController@checklike');
-Route::get('/popage/{id}', 'DreamSharingController@popage');
-// Route::post('/popage', 'DreamSharingController@popage_update');
+//檢查登入，否則導向login
+Route::middleware('auth')->group(function () {
 
-Route::get('/mydream', 'DreamSharingController@mydream');
-Route::get('/mydream_add', 'DreamSharingController@mydream_create');
-Route::post('/mydream_add', 'DreamSharingController@mydream_add');
 
-Route::get('/mydream_result/{id}', 'DreamSharingController@mydream_result');
-// Route::get('/mydream_result/{id}', 'DreamSharingController@mydream_show');
+    Route::get('/', function () {
+        return redirect('/share');
+    });
 
-// Route::delete('/mydream','DreamSharingController@mydream_delete')
+    Route::get('/index', 'HomeController@index');
 
-Route::get('/mydream_edit/{id}', 'DreamSharingController@mydream_edit');
-Route::post('/mydream_edit', 'DreamSharingController@mydream_update');
-Route::get('/mydream_delete/{id}', 'DreamSharingController@mydream_delete');
 
-Route::get('/analyze', 'AnalyzeController@analyze');
-Route::get('/analyze/{kind}', 'AnalyzeController@analyzebykind');
-Route::post('/person', 'AnalyzeController@analyze_person_post');
+    Route::get('/share', 'DreamSharingController@share');
+    Route::get('/share_result/{id}', 'DreamSharingController@share_result');
+    Route::post('/share_result/{id}', 'DreamSharingController@message');
+    Route::get('/keep', 'DreamSharingController@keep');
+    Route::post('/keep', 'DreamSharingController@checklike');
+    Route::get('/popage/{id}', 'DreamSharingController@popage');
+    // Route::post('/popage', 'DreamSharingController@popage_update');
 
-Route::get('/basic_result', 'DreamSharingController@basic');
-Route::get('/basic_update', 'DreamSharingController@basic_update');
-Route::post('/basic_update', 'DreamSharingController@basic_edit');
+    Route::get('/mydream', 'DreamSharingController@mydream');
+    Route::get('/mydream_add', 'DreamSharingController@mydream_create');
+    Route::post('/mydream_add', 'DreamSharingController@mydream_add');
+
+    Route::get('/mydream_result/{id}', 'DreamSharingController@mydream_result');
+    // Route::get('/mydream_result/{id}', 'DreamSharingController@mydream_show');
+
+    // Route::delete('/mydream','DreamSharingController@mydream_delete')
+
+    Route::get('/mydream_edit/{id}', 'DreamSharingController@mydream_edit');
+    Route::post('/mydream_edit', 'DreamSharingController@mydream_update');
+    Route::get('/mydream_delete/{id}', 'DreamSharingController@mydream_delete');
+
+    Route::get('/analyze', 'AnalyzeController@analyze');
+    Route::get('/analyze/{kind}', 'AnalyzeController@analyzebykind');
+    Route::post('/person', 'AnalyzeController@analyze_person_post');
+
+    Route::get('/basic_result', 'DreamSharingController@basic');
+    Route::get('/basic_update', 'DreamSharingController@basic_update');
+    Route::post('/basic_update', 'DreamSharingController@basic_edit');
+});
+
+
+Route::get('/', function () {
+    return redirect('/login');
+});
